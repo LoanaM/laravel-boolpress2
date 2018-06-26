ed4653fd416e6b16bd880733ab31c675677bd408
LoanaM <loanawitch@live.com>
2018-06-26 08:50:56 +0200

    slug create

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
@@ -18,6 +19,27 @@ class CategoryController extends Controller
 
     public function save(Request $request)
     {
+      $request->validate([
+        'name'=>'required|min:4'
+      ]);
+
+      $data = $request->all();
+
+      $data['slug'] = str_slug($data['name']);
+
+      $slugEsistente = Category::where('slug',$data['slug'])->first();
+
+      if(!empty($slugEsistente)){
+        return "Errore! Scegliere un altro nome";
+      }
+
+      $newCategory = new Category();
+
+      $newCategory->fill($data);
+
+      $newCategory->save();
+
+      return redirect()->route('index');
 
     }
 
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,5 +1,18 @@
 @extends('layout')
 
 @section('content')
-  
+  <form class="form-group" action="{{route('save')}}" method="post">
+    {{ csrf_field() }}
+
+    <div class="form-group">
+      <label for="name">Nome</label>
+      <input type="text" name="name" class="form-control" placeholder="Inserisci categoria">
+    </div>
+
+    <div class="form-group">
+      <input type="submit" value="SALVA">
+    </div>
+
+
+  </form>
 @endsection
