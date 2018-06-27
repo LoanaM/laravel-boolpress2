30ec8a8e6a3364135ff55f66f94ab10be22473e0
LoanaM <loanawitch@live.com>
2018-06-28 01:59:56 +0200

    many to many post-catg

--- app/Category.php
+++ app/Category.php
@@ -9,4 +9,11 @@ class Category extends Model
     protected $table = 'categories';
 
     protected $fillable = ['name','slug'];
+
+
+    public function posts()
+    {
+      return $this->belongsToMany('App\post'); //qua aggiungere anche la tabella se non fosse mappata correttamente
+      
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table = 'posts';
+
+    public function categories()
+    {
+      return $this->belongsToMany('App\category'); //qua aggiungere anche la tabella se non fosse mappata correttamente
+      
+    }
 }
